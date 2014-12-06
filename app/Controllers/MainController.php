284451d0973af6df232b9c634f73bf21e2b7af7c
Lacroute <mc.flurry@hotmail.fr>
2014-12-06 20:41:47 +0100

    Handle form security

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -11,7 +11,6 @@ class MainController{
     echo $template->render('layout.htm');
   }
 
-
   
   function initDB(){
     $db=new DB\SQL(
@@ -23,6 +22,47 @@ class MainController{
     );
 
     return $db;
+  }
+
+
+  function displayForm($f3, $params){
+    $type =F3::get('GET')['type'];
+    if($type == "agency"){
+      echo Template::instance()->render('templates/agency_form.htm');
+    }else{
+      echo Template::instance()->render('templates/startup_form.htm');
+    }
+  }
+
+
+  function agencyForm($f3){
+    $clean = $this->securityCheck($f3->get('POST'));
+    return true;
+  }
+
+
+  function startupForm($f3){
+    $clean = $this->securityCheck($f3->get('POST'));
+    return true;
+  }
+
+
+  function securityCheck($data){
+    $clean = array();
+
+    foreach ($data as $key => $value) {
+      if(is_array($value)){
+        $this->securityCheck($value);
+      }else{
+        $value = mysql_real_escape_string($value);
+        $value = strip_tags($value);
+        $value = preg_quote($value);
+        $value = stripslashes($value);
+        $clean[$key] = $value;
+      }
+    }
+
+    return $clean;
   }
 
 
