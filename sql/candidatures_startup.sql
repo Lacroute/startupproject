d3d0be10670df73d54388b0b7538c1c5613451ee
Lacroute <mc.flurry@hotmail.fr>
2014-12-09 23:04:37 +0100

    Merge startup form without files handler

--- sql/candidatures_startup.sql
+++ sql/candidatures_startup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Dim 07 Décembre 2014 à 17:43
+-- Généré le: Mar 09 Décembre 2014 à 22:03
 -- Version du serveur: 5.5.24-log
 -- Version de PHP: 5.4.3
 
@@ -31,8 +31,10 @@ CREATE TABLE IF NOT EXISTS `candidatures_startup` (
   `RS` varchar(250) NOT NULL,
   `nom` varchar(250) NOT NULL,
   `siret` int(14) NOT NULL,
-  `domaine` set('communication digitale/web','création de contenu/interactivité ','événementiel','marketing relationnel/CRM','mobile/applications','Autre') NOT NULL,
   `nature` enum('Auto-entrepreneur','EURL','SA','SARL','SAS','SCOP') NOT NULL,
+  `domaine_creation` varchar(250) NOT NULL,
+  `domaine_analyse` varchar(250) NOT NULL,
+  `domaine_campagnes` varchar(250) NOT NULL,
   `domaine_autre` varchar(250) NOT NULL,
   `rep_prenom` varchar(50) NOT NULL,
   `rep_nom` varchar(70) NOT NULL,
@@ -47,34 +49,28 @@ CREATE TABLE IF NOT EXISTS `candidatures_startup` (
   `date` date NOT NULL,
   `effectif` int(7) NOT NULL,
   `equipe` text NOT NULL,
-  `CA_2011` varchar(20) NOT NULL,
   `CA_2012` varchar(20) NOT NULL,
   `CA_2013` varchar(20) NOT NULL,
   `CA_2014` varchar(20) NOT NULL,
-  `EBE_2011` varchar(20) NOT NULL,
   `EBE_2012` varchar(20) NOT NULL,
   `EBE_2013` varchar(20) NOT NULL,
   `EBE_2014` varchar(20) NOT NULL,
-  `CAE_2011` varchar(20) NOT NULL,
   `CAE_2012` varchar(20) NOT NULL,
   `CAE_2013` varchar(20) NOT NULL,
   `CAE_2014` varchar(20) NOT NULL,
   `description` text NOT NULL,
   `techno` text NOT NULL,
-  `etat` enum('Version Beta','Opérationnelle','Commercialisée') NOT NULL,
+  `innovation` text NOT NULL,
   `atout` text NOT NULL,
   `marche` text NOT NULL,
   `buisnessplan` text NOT NULL,
   `description_GP` text NOT NULL,
   `link` varchar(250) NOT NULL,
   `CG` int(1) NOT NULL DEFAULT '0',
-  `date_inscription` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `valid` int(4) NOT NULL DEFAULT '0',
-  `comment` text NOT NULL,
-  `whish` int(5) NOT NULL,
-  `startup` int(1) NOT NULL DEFAULT '0',
+  `date_inscription` date NOT NULL,
+  `cap_digital_member` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
